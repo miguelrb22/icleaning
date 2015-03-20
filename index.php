712f86a3fdac7447bda3cb251db07d87e3f416a6
Miguel <miguelruizbas@gmail.com>
2015-03-20 16:57:22 +0100

    Nuevo Diseño

--- index.php
+++ index.php
@@ -1,160 +1,266 @@
-<!DOCTYPE html>
+<html lang="es">
+
 <head>
+
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Limpiezas personalizadas</title>
+    <title>iCleaning</title>
 
-    <!-- Bootstrap core CSS -->
+    <!-- Bootstrap CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
 
+    <!-- CSS -->
+    <link href="css/landing.css" rel="stylesheet">
 
-    <link rel="stylesheet" href="css/bootstrap.min.css" />
-    <link rel="stylesheet" href="css/font-awesome.min.css" />
-
-    <link rel="stylesheet" href="https://rawgit.com/FezVrasta/bootstrap-material-design/master/dist/css/material.min.css" />
-    <link rel="stylesheet" href="datepicker/css/bootstrap-material-datepicker.css" />
-    <link href='http://fonts.googleapis.com/css?family=Roboto:400,500' rel='stylesheet' type='text/css'>
-    <link href="css/style.css" rel="stylesheet">
-
+    <!-- Fonts -->
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+    <link href="http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
 
 </head>
 
 <body>
 
-<header class="main-header">
-    <nav class="navbar navbar-default"  id="navmain">
-        <div class="container">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="index.php" style="height:100px">
-                    <div id="pp"> <img src="logo.png" width="192" height="60"></div>
-                </a>
-            </div>
-            <div id="navbar" class="navbar-collapse collapse" style="margin-top:none">
-                <ul class="nav navbar-nav navbar-right" style="margin-top:30px">
-
-                    <li><a href="#"><span id="menuspan">Interiores</span></a></li>
-                    <li><a href="#"><span id="menuspan">Exteriores</span></a></li>
-                    <li><a href="#"><span id="menuspan">Piscinas</span></a></li>
-
-                    <li><button type="button" class="btn btn-success" id="acceso1" data-toggle="modal" data-target="#myModal" data-placement="bottom" title="Acceso franquiciadores" style="margin-top:10px">Área privada</button></li>
-                </ul>
-            </div><!--/.nav-collapse -->
-        </div><!--/.container-fluid -->
-    </nav>
-</header>
-
-
-
-<div class="container">
-    <div class="row ">
-        <form  id="bc" class="form-inline well" role="form">
-            <div style="margin-left: 20%;">
-                
-                <div class="form-group">
-                    <select  style="color:white;"  class="selectbuscador form-control">
-                        <?php require_once 'php/prueba2.php'; ?>
-
-                    </select>
-                </div>
-                
-                <div class="form-group">
-                    <select style="color:white;" class="selectbuscador2 form-control" minlength="60">
-                        <option  style="color:white;" value="">Piscinas</option>
-
-                    </select>
-                </div>
-
-                <div class="form-group">
-                    <input style="color:white;" type="text" id="date-format" class="form-control" placeholder="Fecha">
-                </div>
-
-                <div class="form-group">
-                    <input style="color:white;" type="text" class="form-control" name="busqueda1" placeholder="Buscar..."/>
-                </div>
-
-
-                <button type="submit" class="btn btn-primary">Buscar</button>
-            </div>
-        </form>
+<!-- Navigation -->
+<nav class="navbar navbar-default navbar-fixed-top topnav" role="navigation">
+    <div class="container topnav">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand topnav" href="#">iCleaning</a>
+        </div>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav navbar-right">
+                <li>
+                    <a href="#about">Nosotros</a>
+                </li>
+                <li>
+                    <a href="#services">Servicios</a>
+                </li>
+                <li>
+                    <a href="#contact">Contacto</a>
+                </li>
+            </ul>
+        </div>
+        <!-- /.navbar-collapse -->
     </div>
-</div>
+    <!-- /.container -->
+</nav>
 
 
-<footer class="footer">
+<!-- Header -->
+<a name="about"></a>
+<div class="intro-header">
     <div class="container">
-        <p class="text-muted">iCleaning</p>
+
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="intro-message">
+                    <h1>iCleaning</h1>
+                    <h3>Tu entorno siempre limpio</h3>
+                    <hr class="intro-divider">
+                    <ul class="list-inline intro-social-buttons">
+
+                        <li>
+                            <button class="btn btn-success btn-lg" data-toggle="modal" data-target="#myModal"><i class="fa fa-share"></i> <span class="network-name">Contratar servicios</span></button>
+                        </li>
+
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <!-- /.container -->
+
+</div>
+<!-- /.intro-header -->
+
+<!-- Page Content -->
+
+<a name="services"></a>
+<div class="content-section-a">
+
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-5 col-sm-6">
+                <hr class="section-heading-spacer">
+                <div class="clearfix"></div>
+                <h2 class="section-heading">Interiores</h2>
+                <p class="lead">En iCleaning somos expertos en limpieza de interiores, tanto casas particulares como en oficinas.</p>
+            </div>
+            <div class="col-lg-5 col-lg-offset-2 col-sm-6">
+                <img class="img-responsive" src="oficina.jpg" alt="Oficinas limpias">
+            </div>
+        </div>
+
+    </div>
+    <!-- /.container -->
+
+</div>
+<!-- /.content-section-a -->
+
+<div class="content-section-b">
+
+    <div class="container">
+
+        <div class="row">
+            <div class="col-lg-5 col-lg-offset-1 col-sm-push-6  col-sm-6">
+                <hr class="section-heading-spacer">
+                <div class="clearfix"></div>
+                <h2 class="section-heading">Exteriores</h2>
+                <p class="lead">Tanto interiores, como exteriores. En icleaning limpiamos las ventas de su casa o u oficia por fuera.</p>
+            </div>
+            <div class="col-lg-5 col-sm-pull-6  col-sm-6">
+                <img class="img-responsive" src="exterior.jpg" alt="">
+            </div>
+        </div>
+
+    </div>
+    <!-- /.container -->
+
+</div>
+<!-- /.content-section-b -->
+
+<div class="content-section-a">
+
+    <div class="container">
+
+        <div class="row">
+            <div class="col-lg-5 col-sm-6">
+                <hr class="section-heading-spacer">
+                <div class="clearfix"></div>
+                <h2 class="section-heading">Piscinas</h2>
+                <p class="lead">Si lo que buscas es mantener limpia tu piscina, en icleaning tenemos un servicio inmejorable para ello.</p>
+            </div>
+            <div class="col-lg-5 col-lg-offset-2 col-sm-6">
+                <img class="img-responsive" src="piscina.jpg" alt="">
+            </div>
+        </div>
+
+    </div>
+    <!-- /.container -->
+
+</div>
+<!-- /.content-section-a -->
+
+<a name="contact"></a>
+<div class="banner">
+
+    <div class="container">
+
+        <div class="row">
+            <div class="col-lg-6">
+                <h2>Conecta con iCleaning</h2>
+            </div>
+            <div class="col-lg-6">
+                <ul class="list-inline banner-social-buttons">
+                    <li>
+                        <a href="" class="btn btn-info btn-lg"><i class="fa fa-twitter fa-fw"></i> <span class="network-name">Twitter</span></a>
+                    </li>
+                    <li>
+                        <a href="" class="btn btn-info btn-lg"><i class="fa fa-facebook fa-fw"></i> <span class="network-name">Facebook</span></a>
+                    </li>
+                    <li>
+                        <a href="" class="btn btn-info btn-lg"><i class="fa fa-linkedin fa-fw"></i> <span class="network-name">Linkedin</span></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+    </div>
+    <!-- /.container -->
+
+</div>
+<!-- /.banner -->
+
+<!-- Footer -->
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <ul class="list-inline">
+                    <li>
+                        <a href="#">Inicio</a>
+                    </li>
+                    <li class="footer-menu-divider">⋅</li>
+                    <li>
+                        <a href="#about">Nosotros</a>
+                    </li>
+                    <li class="footer-menu-divider">⋅</li>
+                    <li>
+                        <a href="#services">Servicios</a>
+                    </li>
+                    <li class="footer-menu-divider">⋅</li>
+                    <li>
+                        <a href="#contact">Contacto</a>
+                    </li>
+                </ul>
+                <p class="copyright text-muted small">Copyright © iCleaning 2015. Todos los derechos reservados</p>
+            </div>
+        </div>
     </div>
 </footer>
 
 
+<!-- Ventana modal -->
 <!-- Modal -->
-<div class="modal fade bs-example-modal-sm" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                <h4 class="modal-title" id="myModalLabel">Control de acceso</h4>
+                <h4 class="modal-title" id="myModalLabel">Encuentra lo que buscas facilmente</h4>
             </div>
             <div class="modal-body">
-                <form class="form-inline row" method="post" action="php/logic/login.php">
+                <form class="form-horizontal">
 
-
-                    <div class="form-group col col-lg-12 col-xs-12 col-md-12 col-sm-12">
-                        <label class="sr-only" for="exampleInputAmount">Usuario</label>
-                        <div class="input-group">
-                            <div class="input-group-addon"><i class="fa fa-user-plus"></i>
+                    <div class="form-group">
+                        <div class="col-sm-10 col-sm-offset-1">
+                            <div class="input-group">
+                                <div class="input-group-addon">$</div>
+                                <input type="text" class="form-control" id="example" placeholder="Hola que tal">
                             </div>
-                            <input type="text" name="name" class="form-control" id="Usuariot" placeholder="Usuario">
                         </div>
                     </div>
 
-                    <div class="form-group col col-lg-12 col-xs-12 col-md-12 col-sm-12">
-                        <label class="sr-only" for="exampleInputAmount">Contraseña</label>
-                        <div class="input-group">
-                            <div class="input-group-addon"><i class="fa fa-key"></i></div>
-                            <input type="text" name="pass" class="form-control" id="Contraseña" placeholder="Contraseá">
-                        </div>
-                    </div>
 
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
-                        <button type="submit" class="btn btn-primary">Entrar</button>
+                    <div class="form-group">
+                        <div class="col-sm-10 col-sm-offset-1">
+                            <div class="input-group">
+                                <div class="input-group-addon">$</div>
+                                <input type="text" class="form-control" id="example2" placeholder="Bien y tu">
+                            </div>
+                        </div>
                     </div>
                 </form>
             </div>
-
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+                <button type="button" class="btn btn-primary">¡A Limpiar</button>
+            </div>
         </div>
     </div>
 </div>
 
-<!-- Bootstrap core JavaScript
-================================================== -->
-<script type="text/javascript" src="js/jquery.js"></script>
-<script type="text/javascript" src="js/bootstrap.min.js"></script>
-<script type="text/javascript" src="https://rawgit.com/FezVrasta/bootstrap-material-design/master/dist/js/material.min.js"></script>
-<script type="text/javascript" src="http://momentjs.com/downloads/moment-with-locales.min.js"></script>
-<script type="text/javascript" src="datepicker/js/bootstrap-material-datepicker.js"></script>
-<script src="datepicker/js/bootstrap-material-datepicker.js"></script>
+<!-- jQuery -->
+<script src="js/jquery.js"></script>
+
+<!-- Bootstrap Core JavaScript -->
+<script src="js/bootstrap.min.js"></script>
 
 
-<script type="text/javascript">
 
-    $( document ).ready(function() {
 
-        $('#date-format').bootstrapMaterialDatePicker({ format : 'DD MMMM YYYY', lang: 'es' });
-
-        $.material.init();
-
-    });
-</script>
 </body>
+
 </html>
